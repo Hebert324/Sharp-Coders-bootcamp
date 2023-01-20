645b1d82cd766eb90659ebae2b425b77fd3e9f44
Hebert324 <hebertrocha76@gmail.com>
2023-01-20 09:11:11 -0300

    Adicionando Projetos feitos no curso, Licença do MIT e .gitignore

---
+++ Projetos/ByteBank-POO/ByteBank-POO/Model/Registrar.cs
@@ -0,0 +1,34 @@
+﻿using BytebankPOO.Model;
+
+namespace BytebankPOO.Model
+{
+    public class RegistrarNovoUsuario
+    {
+        public static void Registrar(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
+        {
+            TextoColorido.MudarCorTexto(ConsoleColor.Cyan, "Registrando Usuário");
+
+            Console.Write("Digite o CPF: ");
+            string cpf = Console.ReadLine();
+
+            if (!cpfs.Contains(cpf))
+            {
+                cpfs.Add(cpf);
+
+                Console.Write("Digite o nome: ");
+                titulares.Add(Console.ReadLine());
+
+                Console.Write("Insira sua senha: ");
+                senhas.Add(Console.ReadLine());
+
+                saldos.Add(0);
+
+                TextoColorido.MudarCorTexto(ConsoleColor.Cyan, "Usuário Cadastrado com sucesso!");
+            }
+            else
+            {
+                TextoColorido.MudarCorTexto(ConsoleColor.Red, "CPF já já cadastrado tente outro!");
+            }
+        }
+    }
+}
---
+++ Projetos/ByteBank-POO/ByteBank-POO/View/MenuView.cs
@@ -0,0 +1,47 @@
+﻿namespace BytebankPOO.View
+{
+    public static class MenuView
+    {
+        public static void ShowMenuPrincipal()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("1 - Detalhes de um usuário");
+            Console.WriteLine("2 - Realizar: Depositos, Saques e Transferências");
+            Console.WriteLine("3 - Deletar sua Conta");
+            Console.WriteLine("4 - Listar todas as contas registradas");
+            Console.WriteLine("5 - Quantia total armazenada no banco");
+            Console.WriteLine("0 - Sair da conta");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.Write("Digite a opção desejada: ");
+            Console.WriteLine("");
+        }
+        public static void ShowMenuLogin()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("1 - Criar novo Conta");
+            Console.WriteLine("2 - Fazer Login na Conta");
+            Console.WriteLine("3 - Listar todas as contas registradas");
+            Console.WriteLine("0 - Sair do programa");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.Write("Digite a opção desejada: ");
+            Console.WriteLine("");
+        }
+
+
+        public static void ShowMenuManipularConta()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("1 - Fazer Deposito");
+            Console.WriteLine("2 - Fazer Saque");
+            Console.WriteLine("3 - Fazer Transferência");
+            Console.WriteLine("4 - Verificar Saldo da Conta");
+            Console.WriteLine("0 - Voltar para Menu Principal");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.Write("Digite a opção desejada: ");
+            Console.WriteLine("");
+        }
+    }
+}
